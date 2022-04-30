5522f2501cc8b028f93472f979972fe9c633c6b1
Ajelandro19 <almon190699@gmail.com>
2022-04-29 19:11:14 -0500

    Agregando pruebas de controller verificando que falle una

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,16 @@
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
+
+describe("Tests para ExplorerController",()=>{
+    test("Requerimiento 1: Calcular todos los explorers en una misión", ()=>{
+        const explorersInNode=ExplorerController.getExplorersByMission("node");
+        expect(explorersInNode.length).toBe(10);
+    });
+    test("Requerimiento 2: Obtener el username del explorer en una misión",()=>{
+        const explorersInNode=ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorersInNode[0]).toBe("ajolonauta1");
+    });
+    test("Requerimiento 3: Obtener los explorers que tengan cierto stack",()=>{
+        const explorersInNode=ExplorerController.getExplorersInStack("javascript");
+        expect(explorersInNode[0]).toBe("ajolonauta1");
+    });
+});
